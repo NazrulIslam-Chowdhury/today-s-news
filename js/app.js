03f781dc2851ee731abfcd9bcb21c9aa4187fd51
rahiislam96 <nazrulislam66188@gmail.com>
2022-09-03 17:46:50 +0600

    info added

--- js/app.js
+++ js/app.js
@@ -5,26 +5,29 @@ const loadCategories = async () => {
     displayCategories(data.data.news_category);
 }
 const displayCategories = (categories) => {
+    console.log(categories);
     const categoryContainer = document.getElementById('categories-container');
+
     categories.forEach(category => {
         const categoryPara = document.createElement('li');
-        categoryPara.classList.add('category')
-        categoryPara.innerHTML = `<li>${category.category_name}</li>`;
+        // categoryPara.classList.add('category')
+        categoryPara.innerHTML = `<a class="category text-dark" onclick="loadNews('${category.category_id}')" href="#">${category.category_name}</a>`;
         categoryContainer.appendChild(categoryPara);
 
     });
 }
 
-const loadNews = async () => {
-    const url = `https://openapi.programming-hero.com/api/news/category/01`;
+const loadNews = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${id}`;
     const res = await fetch(url);
     const data = await res.json();
     displayNews(data.data);
 }
 const displayNews = (newes) => {
     const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = '';
     newes.forEach(news => {
-        // console.log(news);
+        console.log(news);
         const newsDiv = document.createElement('div');
         newsDiv.classList.add('card');
         newsDiv.innerHTML = `
@@ -34,10 +37,18 @@ const displayNews = (newes) => {
                         </div>
                         <div class="col-md-8">
                             <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural
-                                    lead-in to
-                                    additional content. This content is a little bit longer.</p>
+                                <h5 class="card-title fw-bold">${news.title}</h5>
+                                <p class="card-text mt-4">${news.details.slice(0, 300)}...</p>
+                        <div class="d-flex>
+                            <div class="author-img w-25 h-25">
+                                <img src="${news.author.img}alt="...">
+                            </div>
+                             <div>
+                                <p>${news.author.name}</p>
+                                <p>${news.author.published_date}</p>
+                            </div>
+                            <p>${news.total_view}</p>
+                        </div>
                                 <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
@@ -46,5 +57,5 @@ const displayNews = (newes) => {
         newsContainer.appendChild(newsDiv);
     });
 }
-loadNews()
-loadCategories();
+
+loadCategories('Breaking News');
